b035fdb7792f82f1e35295767fc996aceda751ea
Olivia <olitoxqui@gmail.com>
2022-08-16 23:26:30 -0500

    Update del 16082022

---
+++ IntelliJ Proyects/prueba/src/main/java/generation/example/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo{
+    public static void main(String[] args){
+        String miVariable="Hola Erick";
+        System.out.println(miVariable);
+    }
+}
---
+++ ejemplo.jdk/Hola.java
@@ -0,0 +1,12 @@
+public class Hola{
+//esto es una clase del JDK Java	
+
+	public static void main (String [] arg){
+	String miVariable="Hola mundo, un saludito a: ";
+	String miNombre="Olivia GT";
+
+	//Es la declaración del método main
+		System.out.println (miVariable+miNombre);
+	}
+
+}
